ebb1609b4aed8b0462c8c0ad4b533fedd7e647c6
Aleksandr Zhukov <ferruspoint@mail.ru>
2024-06-29 17:52:50 +0700

    Fix syntax errors in PlayerController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,11 @@ public class PlayerController : MonoBehaviour
 void Start()
     {rb = GetComponent<Rigidbody2D>(); }
 
-    void Update()
-    {float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        Vector2m movement = new Vector2m(moveX, moveY;
-        rb.velocity = movement * moveSpeed;
-    }
+void Update()
+{
+    float moveX = Input.GetAxis("Horizontal");
+    float moveY = Input.GetAxis("Vertical");
+    Vector2m movement = new Vector2m(moveX, moveY);
+    rb.velocity = movement * moveSpeed;
+}
 }
