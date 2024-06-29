92fa26705830d7f2bf550166ac4e70be888b74a8
Aleksandr Zhukov <ferruspoint@mail.ru>
2024-06-29 17:53:26 +0700

    Fix syntax errors in PlayerCollision script

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,12 +2,13 @@ using Unity;
 
 public class PlayerCollision : MonoBehavious
 {
+
 void OnCollisionEnter2D(Collision2D collision)
 {
     if (collision.gameObject.CompareTag("Obstacle"))
     {
-        // Logica pri stolknovenii s prepåatstviem
-        Debug.Log("Stolknovenie s prepåatstviem!");
+        // Logica pristolknovenia prepyastviem
+        Debug.Log("Stolknovenia prepyastvim!");
     }
 }
 }
