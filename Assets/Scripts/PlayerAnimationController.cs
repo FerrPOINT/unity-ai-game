5c095550a87076890560fd4f88b24d7fa0850fdf
Aleksandr Zhukov <ferruspoint@mail.ru>
2024-06-29 17:53:09 +0700

    Fix syntax errors in PlayerAnimationController script

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -9,11 +9,12 @@ void Start()
     {animator = GetComponent<Animator>(); }
 
 void Update()
-    {float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        movement = new Vector2m(moveX, moveY;
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
+{
+    float moveX = Input.GetAxis("Horizontal");
+    float moveY = Input.GetAxis("Vertical");
+    movement = new Vector2m(moveX, moveY);
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
+}
 }
